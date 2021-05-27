f30f28ff87d8da23180e2a842c6fe4481c8ab3e9
Eimhin Ward <eimhinward@gmail.com>
2021-05-27 21:44:49 +0100

    Initial Commit

---
+++ swift_stanford_memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_stanford_memorize
+//
+//  Created by Éimhín Ward  on 27/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_stanford_memorize/swift_stanford_memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_stanford_memorizeApp.swift
+//  swift_stanford_memorize
+//
+//  Created by Éimhín Ward  on 27/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_stanford_memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
